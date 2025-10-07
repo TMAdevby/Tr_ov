210530e64d767adfcd032fe3a725a0d7e7a4bf4a
TMAdevby <mtipun@mail.ru>
2025-10-07 22:58:54 +0300

    FileWriter2

--- src/main/java/bl_belt/work_with_files/FileWriterEx.java
+++ src/main/java/bl_belt/work_with_files/FileWriterEx.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 
 public class FileWriterEx {
     public static void main(String[] args) throws IOException {
-        String rubai = "Много лет размышлял я над жизнью земной." +
-                "Непонятного нет для меня под луной." +
-                "Мне известно что мне ничего не известно!" +
-                "Вот последняя правда открытая мной.";
+        String rubai = "Много лет размышлял я над жизнью земной.\n" +
+                "Непонятного нет для меня под луной.\n" +
+                "Мне известно что мне ничего не известно!\n" +
+                "Вот последняя правда открытая мной.\n";
         FileWriter writer = null;
         try{
             //writer = new FileWriter("C:\\Users\\Lenovo\\OneDrive\\Desktop\\text1.txt", true);
-            writer = new FileWriter("text2.txt");
+            writer = new FileWriter("text2.txt",false);
             /*for(int i = 0; i<rubai.length(); i++){
                 writer.write(rubai.charAt(i));
             }*/
