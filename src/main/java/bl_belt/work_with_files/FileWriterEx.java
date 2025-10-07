d8acfe2be7c43a063fd4d3752f2aa2253e7890ca
TMAdevby <mtipun@mail.ru>
2025-10-07 22:53:13 +0300

    FileWriter

---
+++ src/main/java/bl_belt/work_with_files/FileWriterEx.java
@@ -0,0 +1,28 @@
+package bl_belt.work_with_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterEx {
+    public static void main(String[] args) throws IOException {
+        String rubai = "Много лет размышлял я над жизнью земной." +
+                "Непонятного нет для меня под луной." +
+                "Мне известно что мне ничего не известно!" +
+                "Вот последняя правда открытая мной.";
+        FileWriter writer = null;
+        try{
+            writer = new FileWriter("C:\\Users\\Lenovo\\OneDrive\\Desktop\\text1.txt", true);
+            /*for(int i = 0; i<rubai.length(); i++){
+                writer.write(rubai.charAt(i));
+            }*/
+            writer.write(rubai);
+            writer.write("\n");
+            System.out.println("Done!");
+        }   catch (IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            writer.close();
+        }
+    }
+}
