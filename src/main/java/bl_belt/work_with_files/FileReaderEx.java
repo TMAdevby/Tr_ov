7f9d1b6f312c1536d8f6711ce2bb267c3a90a7e4
TMAdevby <mtipun@mail.ru>
2025-10-08 15:24:55 +0300

    with resources

--- src/main/java/bl_belt/work_with_files/FileReaderEx.java
+++ src/main/java/bl_belt/work_with_files/FileReaderEx.java
@@ -2,13 +2,13 @@ package bl_belt.work_with_files;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileReaderEx {
     public static void main(String[] args) throws IOException {
-        FileReader reader = null;
-        try {
-            reader =new FileReader("text2.txt");
+        try (FileReader reader =new FileReader("text2.txt");
+             FileWriter writer = new FileWriter("text2.txt", true) ) {
             int character;
             while ((character = reader.read()) != -1) {
                 System.out.print((char)character);
@@ -18,8 +18,6 @@ public class FileReaderEx {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        finally {
-            reader.close();
-        }
+
     }
 }
---
+++ src/main/java/bl_belt/work_with_files/Withresources.java
@@ -0,0 +1,27 @@
+package bl_belt.work_with_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Withresources {
+    public static void main(String[] args) throws IOException {
+        String rubai = "Много лет размышлял я над жизнью земной.\n" +
+                "Непонятного нет для меня под луной.\n" +
+                "Мне известно что мне ничего не известно!\n" +
+                "Вот последняя правда открытая мной.\n";
+        String s = "privet";
+
+        try(FileWriter writer = new FileWriter("text2.txt", true) ){
+
+            /*for(int i = 0; i<rubai.length(); i++){
+                writer.write(rubai.charAt(i));
+            }*/
+            writer.write(rubai);
+            writer.write(s);
+            System.out.println("Done!");
+        }   catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
