89b3ec9cefa383d3531bb55cd981b1beae2b8646
TMAdevby <mtipun@mail.ru>
2025-10-09 15:10:10 +0300

    Serialization 1

---
+++ src/main/java/bl_belt/work_with_files/programmer2/SerializationEx1.java
@@ -0,0 +1,26 @@
+package bl_belt.work_with_files.programmer2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerializationEx1 {
+    public static void main(String[] args) {
+        List employees;
+        try (ObjectInputStream inputStream = new ObjectInputStream(
+                new FileInputStream("employees1.bin")
+        )) {
+            employees = (ArrayList)inputStream.readObject();
+            System.out.println(employees);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
