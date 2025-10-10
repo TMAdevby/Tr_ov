29d50e07fb44adfef1eefd837ea43e49fbd180d1
TMAdevby <mtipun@mail.ru>
2025-10-10 18:18:26 +0300

    Buffers Channels write

--- src/main/java/bl_belt/work_with_files_NIO/ChannelBufferEx1.java
+++ src/main/java/bl_belt/work_with_files_NIO/ChannelBufferEx1.java
@@ -30,6 +30,17 @@ public class ChannelBufferEx1 {
             }
             System.out.println(stix);
 
+    String text = "\nThere are only two ways to live your life." +
+            " One is as though nothing is a miracle. The other is as " +
+            " though everything is a miracle. ";
+
+    /*ByteBuffer buffer2 = ByteBuffer.allocate(text.getBytes().length);
+    buffer2.put(text.getBytes());
+    buffer2.flip();
+    channel.write(buffer2);*/
+
+    ByteBuffer buffer2 = ByteBuffer.wrap(text.getBytes());
+    channel.write(buffer2);
 
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
