ec0af76bb5a5b977a4a887f52ca332e612cdf957
TMAdevby <mtipun@mail.ru>
2025-11-02 14:04:19 +0300

    mult7

--- src/main/java/bl_belt/multithreading/o2_methodsTrhread/Ex8.java
+++ src/main/java/bl_belt/multithreading/o2_methodsTrhread/Ex8.java
@@ -13,12 +13,14 @@ public class Ex8 extends Thread {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread1 = new Thread(new MyRunnable1());
         Ex8 thread2 = new Ex8();
         thread1.start();
         thread2.start();
 
+        thread1.join();
+        thread2.join();
         System.out.println("Konec!");
     }
 }
