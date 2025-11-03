d1c596c917b6f351a5ff9a5efcbd1987fe97f365
TMAdevby <mtipun@mail.ru>
2025-11-03 21:30:40 +0300

    wait

---
+++ src/main/java/bl_belt/multithreading/o4_waitNotify/WaitNotifyExample.java
@@ -0,0 +1,25 @@
+package bl_belt.multithreading.o4_waitNotify;
+
+public class WaitNotifyExample {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Market{
+    private int breadCount = 0;
+
+    public synchronized void getBread(){
+        while(breadCount<1){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        breadCount--;
+        System.out.println("Потребитель купил 1 хлеб");
+        System.out.println("Количество хлеба в магазине = " + breadCount);
+        notify();
+    }
+}
