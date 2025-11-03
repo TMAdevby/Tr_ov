50072793f9c7280a9041abba9ba62279632d8917
TMAdevby <mtipun@mail.ru>
2025-11-03 18:36:24 +0300

    mult16

--- src/main/java/bl_belt/multithreading/o3_vol_synchronized/Ex12.java
+++ src/main/java/bl_belt/multithreading/o3_vol_synchronized/Ex12.java
@@ -1,41 +1,51 @@
 package bl_belt.multithreading.o3_vol_synchronized;
 
 public class Ex12 {
+    static final Object lock = new Object();
 
     void mobileCall() {
-        System.out.println("Mobile call starts");
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        synchronized (lock) {
+            System.out.println("Mobile call starts");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Mobile call ends");
         }
-        System.out.println("Mobile call ends");
     }
 
     void skypeCall() {
-        System.out.println("Skype call starts");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        synchronized (lock) {
+            System.out.println("Skype call starts");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Skype call ends");
         }
-        System.out.println("Skype call ends");
     }
 
     void whatsappCall() {
-        System.out.println("Whatsapp call starts");
-        try {
-            Thread.sleep(7000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        synchronized (lock) {
+            System.out.println("Whatsapp call starts");
+            try {
+                Thread.sleep(7000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Whatsapp call ends");
         }
-        System.out.println("Whatsapp call ends");
     }
 
     public static void main(String[] args) {
         Thread thread1 = new Thread(new RunnableImplMobile());
-        Thread thread2 = new Thread(new RunnableImplMobile());
-        Thread thread3 = new Thread(new RunnableImplMobile());
+        Thread thread2 = new Thread(new RunnableImplSkype());
+        Thread thread3 = new Thread(new RunnableImplWhatsapp());
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
 
@@ -58,4 +68,8 @@ class RunnableImplWhatsapp implements Runnable{
     public void run() {
         new Ex12().whatsappCall();
     }
+}
+
+class Car{
+
 }
