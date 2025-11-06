254e29fbe22cdb37ae3f6effc57c5a2862a57a44
TMAdevby <mtipun@mail.ru>
2025-11-06 16:08:52 +0300

    wait3

--- src/main/java/bl_belt/multithreading/o4_waitNotify/WaitNotifyExample.java
+++ src/main/java/bl_belt/multithreading/o4_waitNotify/WaitNotifyExample.java
@@ -2,7 +2,13 @@ package bl_belt.multithreading.o4_waitNotify;
 
 public class WaitNotifyExample {
     public static void main(String[] args) {
-
+        Market market = new Market();
+        Producer producer = new Producer(market);
+        Consumer consumer = new Consumer(market);
+        Thread thread1 = new Thread(producer);
+        Thread thread2 = new Thread(consumer);
+        thread1.start();
+        thread2.start();
     }
 }
 
@@ -33,6 +39,35 @@ class Market{
         }
         breadCount++;
         System.out.println("Производитель добавил на витрину 1 хлеб");
+        System.out.println("Количество хлеба в магазине = " + breadCount);
+        notify();
+    }
+}
 
+class Producer implements Runnable{
+    Market market;
+    Producer(Market market){
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.putBread();
+        }
+    }
+}
+
+class Consumer implements Runnable{
+    Market market;
+    Consumer(Market market){
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.getBread();
+        }
     }
 }
