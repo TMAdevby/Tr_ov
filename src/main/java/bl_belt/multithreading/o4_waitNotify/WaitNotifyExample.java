9099e9d0f83e03197584323fe3965e9208b29787
TMAdevby <mtipun@mail.ru>
2025-11-06 15:49:33 +0300

    wait2

--- src/main/java/bl_belt/multithreading/o4_waitNotify/WaitNotifyExample.java
+++ src/main/java/bl_belt/multithreading/o4_waitNotify/WaitNotifyExample.java
@@ -22,4 +22,17 @@ class Market{
         System.out.println("Количество хлеба в магазине = " + breadCount);
         notify();
     }
+
+    public synchronized void putBread(){
+        while (breadCount>=5){
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        breadCount++;
+        System.out.println("Производитель добавил на витрину 1 хлеб");
+
+    }
 }
