c15ab9e619ed72ee355d7698962093e6d7f3c1e0
TMAdevby <mtipun@mail.ru>
2025-11-07 12:34:29 +0300

    deadlock3

--- src/main/java/bl_belt/multithreading/o5_deadlock/DeadLockEx.java
+++ src/main/java/bl_belt/multithreading/o5_deadlock/DeadLockEx.java
@@ -3,8 +3,14 @@ package bl_belt.multithreading.o5_deadlock;
 public class DeadLockEx {
     public static final Object lock1 = new Object();
     public static final Object lock2 = new Object();
-}
 
+    public static void main(String[] args) {
+        Thread10 thread10 = new Thread10();
+        Thread20 thread20 = new Thread20();
+        thread10.start();
+        thread20.start();
+    }
+}
 
 class Thread10 extends Thread{
     @Override
@@ -15,6 +21,21 @@ class Thread10 extends Thread{
             System.out.println("Thread10: Попытка захватить монитор объекта lock2");
             synchronized (DeadLockEx.lock2){
                 System.out.println("Thread10: Мониторы объектов lock1 " +
+                        "и lock2 захвачены");
+            }
+        }
+    }
+}
+
+class Thread20 extends Thread{
+    @Override
+    public void run() {
+        System.out.println("Thread20: Попытка захватить монитор объекта lock2");
+        synchronized (DeadLockEx.lock2){
+            System.out.println("Thread20: Монитор объекта lock2 захвачен");
+            System.out.println("Thread20: Попытка захватить монитор объекта lock1");
+            synchronized (DeadLockEx.lock1){
+                System.out.println("Thread20: Мониторы объектов lock1 " +
                         "и lock2 захвачены");
             }
         }
