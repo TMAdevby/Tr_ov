6966652a9067dc2e83230a46ef41c6db2cfd5fe5
TMAdevby <mtipun@mail.ru>
2025-11-07 16:46:28 +0300

    lock4

--- src/main/java/bl_belt/multithreading/o6_lock/Bankomat.java
+++ src/main/java/bl_belt/multithreading/o6_lock/Bankomat.java
@@ -1,4 +1,39 @@
 package bl_belt.multithreading.o6_lock;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Bankomat {
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        new Employee("Zaur", lock);
+        new Employee("Oleg", lock);
+        new Employee("Elena", lock);
+        new Employee("Viktor", lock);
+        new Employee("Marina", lock);
+    }
+}
+
+class Employee extends Thread{
+    String name;
+    private Lock lock;
+    public Employee(String name, Lock lock){
+        this.name = name;
+        this.lock = lock;
+        this.start();
+    }
+    public void run(){
+        try {
+        System.out.println(name + " ждет... ");
+        lock.lock();
+        System.out.println(name + " пользуется банкоматом... ");
+            Thread.sleep(2000);
+            System.out.println(name + " завершил свои дела ");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            lock.lock();
+        }
+    }
 }
