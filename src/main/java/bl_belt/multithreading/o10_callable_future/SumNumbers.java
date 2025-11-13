d53d5d3c63bd6e13a53abb89b950de5af5452607
TMAdevby <mtipun@mail.ru>
2025-11-13 16:04:18 +0300

    Collable5

--- src/main/java/bl_belt/multithreading/o10_callable_future/SumNumbers.java
+++ src/main/java/bl_belt/multithreading/o10_callable_future/SumNumbers.java
@@ -1,13 +1,29 @@
 package bl_belt.multithreading.o10_callable_future;
 
-import java.util.concurrent.Callable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class SumNumbers {
     private static long value = 1_000_000_000L;
     private static long sum = 0;
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future<Long>> futureResults = new ArrayList<>();
+        long valueDividedBy10 = value/10;
+        for(int i = 0; i < 10; i++){
+            long from = valueDividedBy10*i+1;
+            long to = valueDividedBy10*(i+1);
+            PartialSum task = new PartialSum(from,to);
+            Future<Long> futurePartSum = executorService.submit(task);
+            futureResults.add(futurePartSum);
+        }
+        for(Future<Long>result : futureResults){
+            sum += result.get();
+        }
+        executorService.shutdown();
+        System.out.println("Total sum = " + sum);
     }
 }
 
@@ -23,6 +39,7 @@ class PartialSum implements Callable<Long>{
         for(long i = from; i<=to; i++){
             localSum += i;
         }
-        System.out.println("Sum from " + from + " to" + to + " = " + localSum);
+        System.out.println("Sum from " + from + " to " + to + " = " + localSum);
+        return localSum;
     }
 }
