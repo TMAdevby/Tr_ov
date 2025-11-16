a3aba3a6ae1a4c4493eec7002c952d1c854f1fcf
TMAdevby <mtipun@mail.ru>
2025-11-16 20:33:00 +0300

    CountDownLatch2

--- src/main/java/bl_belt/multithreading/o12_countDownLatch/CountDownLatchEx.java
+++ src/main/java/bl_belt/multithreading/o12_countDownLatch/CountDownLatchEx.java
@@ -1,4 +1,41 @@
 package bl_belt.multithreading.o12_countDownLatch;
 
+import java.util.concurrent.CountDownLatch;
+
 public class CountDownLatchEx {
+    static CountDownLatch countDownLatch = new CountDownLatch(3);
+
+    private static void marketStaffIsOnPlace() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("Market staff came to work");
+        countDownLatch.countDown();
+        System.out.println("countDownLatch = " + countDownLatch.getCount());
+    }
+
+    private static void everythingIsReady() throws InterruptedException {
+        Thread.sleep(3000);
+        System.out.println("Everything is ready, so let's open market");
+        countDownLatch.countDown();
+        System.out.println("countDownLatch = " + countDownLatch.getCount());
+    }
+
+    private static void openMarket() throws InterruptedException {
+        Thread.sleep(4000);
+        System.out.println("Market is opened");
+        countDownLatch.countDown();
+        System.out.println("countDownLatch = " + countDownLatch.getCount());
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Friend extends Thread{
+    String name;
+    private CountDownLatch countDownLatch;
+    public Friend(String name, CountDownLatch countDownLatch){
+        this.name = name;
+        this.countDownLatch = countDownLatch;
+    }
 }
