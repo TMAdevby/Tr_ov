c6b72f507d8f2566c5e011ce623515316c50116b
TMAdevby <mtipun@mail.ru>
2025-11-19 21:20:45 +0300

    Nested2

---
+++ src/main/java/bl_belt/Nested_clsses/static_nested_class/Car.java
@@ -0,0 +1,43 @@
+package bl_belt.Nested_clsses.static_nested_class;
+
+public class Car {
+    String color;
+    int doorCount;
+    Engine engine;
+
+    public Car(String color, int doorCount, Engine engine){
+        this.color = color;
+        this.doorCount = doorCount;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "My car: {" +
+                "color='" + color + '\'' +
+                ", doorCount=" + doorCount +
+                ", engine=" + engine +
+                '}';
+    }
+
+    public static class Engine{
+        int horsePower;
+
+        public Engine(int horsePower) {
+            this.horsePower = horsePower;
+        }
+
+        @Override
+        public String toString() {
+            return "My engine: {" +
+                    "horsePower=" + horsePower +
+                    '}';
+        }
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+
+    }
+}
