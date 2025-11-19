840bdd5163796151788e30a84687dfb05f883c94
TMAdevby <mtipun@mail.ru>
2025-11-19 18:02:33 +0300

    CopyOnWriteArrayList

---
+++ src/main/java/bl_belt/multithreading/o17_copyOnWriteArraylistEx/CopyOnWriteArrayListEx.java
@@ -0,0 +1,22 @@
+package bl_belt.multithreading.o17_copyOnWriteArraylistEx;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class CopyOnWriteArrayListEx {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Zaur");
+        list.add("Oleg");
+        list.add("Sergey");
+        list.add("Ivan");
+        list.add("Igor");
+        System.out.println(list);
+
+        Runnable runnable1 = () -> {
+            Iterator<String> iterator = list.iterator();
+
+        }
+
+    }
+}
