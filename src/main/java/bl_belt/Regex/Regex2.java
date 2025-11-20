db4b2fcb3bb8b2e10870608bf3f2f87819a8fbf4
TMAdevby <mtipun@mail.ru>
2025-11-20 17:55:36 +0300

    REGEX

---
+++ src/main/java/bl_belt/Regex/Regex2.java
@@ -0,0 +1,29 @@
+package bl_belt.Regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex2 {
+    public static void main(String[] args) {
+//        String s1 = "ABCD ABCE ABCFABCGABCH";
+//        Pattern pattern1 = Pattern.compile("ABC");
+//        String s1 = "OPABMNCD";
+//        Pattern pattern1 = Pattern.compile("[ABC]");
+//        String s1 = "ABDOP";
+//        Pattern pattern1 = Pattern.compile("AB[C-F]OP");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc[e-g4-7]");
+
+//        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc[^e-g4-7]");
+
+        String s1 = "abcd abce abc5abcg6abch";
+        Pattern pattern1 = Pattern.compile("abc(e|5)");
+
+        Matcher mather = pattern1.matcher(s1);
+        while (mather.find()){
+            System.out.println("Position: " + mather.start() + " " + mather.group());
+        }
+    }
+}
