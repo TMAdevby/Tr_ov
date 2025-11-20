c3e3a31fa72d78b76235378b4c595468570c733a
TMAdevby <mtipun@mail.ru>
2025-11-20 13:56:55 +0300

    local inner3

---
+++ src/main/java/bl_belt/Nested_clsses/local_inner_classes/LocalInner2.java
@@ -0,0 +1,20 @@
+package bl_belt.Nested_clsses.local_inner_classes;
+
+public class LocalInner2 {
+    public static void main(String[] args) {
+        class Slojenie implements Math2{
+
+            @Override
+            public int doOperation(int a, int b) {
+                return a + b;
+            }
+        }
+        Slojenie s = new Slojenie();
+        System.out.println(s.doOperation(5,3));
+    }
+}
+
+interface Math2{
+    int doOperation(int a, int b);
+
+}
