25c0dabcc1cd2503b1fbcde893ca05bd492bdd71
TMAdevby <mtipun@mail.ru>
2025-11-21 21:19:04 +0300

    REGEX7

---
+++ src/main/java/bl_belt/Regex/Regex4.java
@@ -0,0 +1,13 @@
+package bl_belt.Regex;
+
+public class Regex4 {
+    public static void main(String[] args) {
+        String s1 = "Privet,     moy  drug!   Kak idet izuchenie    Java        ?";
+        System.out.println(s1);
+//        s1 = s1.replace("Java", "SQL");
+//        s1 = s1.replaceAll(" {2,}", " ");
+        s1 = s1.replaceAll("\\bi\\w+", "4444");
+        System.out.println(s1);
+
+    }
+}
