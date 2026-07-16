8a655be11e09a65905318add16ded830eb021b7b
TMAdevby <mtipun@mail.ru>
2026-07-16 21:00:42 +0300

    reflection2

--- src/main/java/bl_belt/Reflection/Ex1.java
+++ src/main/java/bl_belt/Reflection/Ex1.java
@@ -1,5 +1,29 @@
 package bl_belt.Reflection;
 
-public class Ex1 {
+import java.lang.reflect.Field;
 
+public class Ex1 {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException {
+        Class employeeClass = Class.forName("bl_belt.Reflection.Employee");
+//        Class employeeClass2 = Employee.class;
+//        Employee emp = new Employee();
+//        Class empliyeeClass = emp.getClass();
+
+        Field sameField = employeeClass.getField("id");
+        System.out.println("Type of id field " + sameField.getType());
+        System.out.println("********");
+
+        Field [] fields = employeeClass.getFields();
+        for(Field field : fields){
+            System.out.println("Type of " + field.getName() + " = " + field.getType());
+        }
+        System.out.println("********");
+
+        Field [] allFields = employeeClass.getDeclaredFields();
+        for(Field field : allFields){
+            System.out.println("Type of " + field.getName() + " = " + field.getType());
+        }
+
+
+    }
 }
