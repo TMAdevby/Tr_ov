6d1299eb641074a0219c3b39f11f42ae2468770f
TMAdevby <mtipun@mail.ru>
2026-07-17 20:59:08 +0300

    reflection7

--- src/main/java/bl_belt/Reflection/Ex2.java
+++ src/main/java/bl_belt/Reflection/Ex2.java
@@ -2,6 +2,7 @@ package bl_belt.Reflection;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Ex2 {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
@@ -12,7 +13,13 @@ public class Ex2 {
 
         Constructor constructor2 = employeeClass.getConstructor(int.class,
                 String.class, String.class);
-        Object obj2 = constructor2.newInstance();
+        Object obj2 = constructor2.newInstance(5,"Zaur","IT");
+        System.out.println(obj2);
+
+        Method method = employeeClass.getMethod("setSalary", double.class);
+        method.invoke(obj2, 800.88);
+        System.out.println(obj2);
+
 
     }
 }
