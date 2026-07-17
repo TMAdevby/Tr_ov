bc7dd473365d5e0b0590bbc6e1a4788487b5493a
TMAdevby <mtipun@mail.ru>
2026-07-17 21:37:10 +0300

    reflection10

--- src/main/java/bl_belt/Reflection/Calculator.java
+++ src/main/java/bl_belt/Reflection/Calculator.java
@@ -1,5 +1,12 @@
 package bl_belt.Reflection;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Calculator {
     void sum(int a, int b){
         int result = a + b;
@@ -19,5 +26,40 @@ public class Calculator {
     void division(int a, int b){
         int result = a / b;
         System.out.println("Частное " + a + " и " + b + " = " + result);
+    }
+}
+
+class TestCalculator {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader("test100.txt"))) {
+            String methodName = reader.readLine();
+            String firstArgument = reader.readLine();
+            String secondArgument = reader.readLine();
+
+            Calculator calculator = new Calculator();
+            Class c1 = calculator.getClass();
+            Method method = null;
+
+            Method[] methods = c1.getDeclaredMethods();
+            for (Method myMethod : methods) {
+                if (myMethod.getName().equals(methodName)) {
+                    method = myMethod;
+                }
+            }
+
+            method.invoke(calculator, Integer.parseInt(firstArgument),
+                    Integer.parseInt(secondArgument));
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
